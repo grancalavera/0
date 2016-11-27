0c04f22e9065b6320c5f218e57e76f30f57a9645
Leon Coto <leoncoto@gmail.com>
2016-11-27 16:50:50 +0000

    Cleanup

--- config.postcss.js
+++ config.postcss.js
@@ -3,8 +3,11 @@ module.exports = {
   [ 'postcss-import'
   , 'postcss-cssnext'
   ]
-, input: ['src/app.css', 'src/lib.css']
-, dir: 'dist/'
+, input:
+  [ 'src/app.css'
+  , 'src/lib.css'
+  ]
+, dir: 'dist'
 , map: 'file'
 , 'local-plugins': true
 }
